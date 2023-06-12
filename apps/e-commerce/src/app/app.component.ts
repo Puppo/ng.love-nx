9938fdc653532ea5d7a96752c84324b0f736a3af
Luca Del Puppo <luca@delpuppo.net>
2023-06-12 14:29:58 +0200

    01 - add first application

---
+++ apps/e-commerce/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  selector: 'ngl-root',
+  template: `<router-outlet></router-outlet>`,
+})
+export class AppComponent {}
